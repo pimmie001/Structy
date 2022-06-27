f60e15e7b660924079712f5b00415bcf61d0f3ab
Pim <pimvkeulen@gmail.com>
2022-06-27 16:15:56 +0200

    depth first values

---
+++ binary tree/depth_first_values.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def depth_first_values(root, values=[]):
+    if root is not None:
+        values.append(root.val)
+    else:
+        return values
+
+    if root.left is not None:
+        values = depth_first_values(root.left, values)
+    if root.right is not None:
+        values = depth_first_values(root.right, values)
+
+    return values
+
