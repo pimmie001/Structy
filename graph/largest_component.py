1842da2efafb64abecea07b5f6afbf909602bd10
Pim <pimvkeulen@gmail.com>
2022-06-29 20:31:27 +0200

    graph: largest component

--- graph/largest_component.py
+++ graph/largest_component.py
@@ -1,7 +1,3 @@
-import numpy as np
-from connected_components_count import get_connected_components
-
-
 def largest_component(graph):
     # returns the size of the largest component in a graph
 
@@ -9,38 +5,25 @@ def largest_component(graph):
     largest = 0
     while nodes:
         components = get_connected_components(graph, nodes[0])
-        print(nodes[0], components)
-        # print(get_connected_components(graph, 4))
 
-        sizes.append(len(components))
+        if len(components) > largest:
+            largest = len(components)
 
         for node in components:
             if node in nodes:
                 nodes.remove(node)
 
-    return np.max(sizes)
+    return largest
 
 
 
-(largest_component({
-    3: [],
-    4: [6],
-    6: [4, 5, 7, 8],
-    8: [6],
-    7: [6],
-    5: [6],
-    1: [2],
-    2: [1]
-})) # -> 4
-
-
-
-
-
-
-
-
-
-
-
+def get_connected_components(graph, node):
+    stack = [node]
+    components = []
+    while stack:
+        current = stack.pop()
+        if current not in components:
+            components.append(current)
+            stack.extend(graph[current])
+    return components
 
