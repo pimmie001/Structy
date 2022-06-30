a010bce1bc1792e70908d3f8ee4541f74abdc894
Pim <pimvkeulen@gmail.com>
2022-06-30 03:13:45 +0200

    update dp non ajacent sum

--- dynamic programming/non_adjacent_sum.py
+++ dynamic programming/non_adjacent_sum.py
@@ -5,15 +5,12 @@ def non_adjacent_sum(nums):
     if len(nums) == 1:
         return nums[0]
 
-    if len(nums) == 2:
-        return max(nums[0], nums[1])
-    
 
-    n = len(nums)//2 # index of middle 
+    n = len(nums)//2 # index of the middle number
     left = nums[:n]
     mid = nums[n]
     right = nums[n+1:]
 
     return max(non_adjacent_sum(left) + non_adjacent_sum(right), non_adjacent_sum(left[:-1]) + mid + non_adjacent_sum(right[1:]))
-    # return max('do not use the middle', 'use the middle')
+    # return max('do not include the middle', 'include the middle')
 
