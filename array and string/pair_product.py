e385fab2b04359b57c9ff051f29d74cb2b0eb9bd
Pim <pimvkeulen@gmail.com>
2022-07-03 18:23:58 +0200

    improve efficiency pair sum/product

--- array and string/pair_product.py
+++ array and string/pair_product.py
@@ -1,7 +1,23 @@
 def pair_product(numbers, target_sum):
+    # time O(n^2), space O(1)
+
     for i in range(len(numbers)):
         n = numbers[i]
         for j in range(i + 1, len(numbers)):
             m = numbers[j]
             if n * m == target_sum:
                 return (i,j)
+
+
+def pair_product(numbers, target_sum):
+    # time O(n), space O(n)
+
+    memo = {}
+
+    for i, number in enumerate(numbers):
+        complement = target_sum/number
+
+        if complement in memo:
+            return (memo[complement], i)
+        
+        memo[number] = i
--- array and string/pair_sum.py
+++ array and string/pair_sum.py
@@ -1,7 +1,24 @@
 def pair_sum(numbers, target_sum):
+    # time O(n^2), space O(1)
+
     for i in range(len(numbers)):
         n = numbers[i]
         for j in range(i+1, len(numbers)):
             m = numbers[j]
             if n + m == target_sum:
                 return (i,j)
+
+
+def pair_sum(numbers, target_sum):
+    # time O(n), space O(n)
+
+    memo = {}
+    for i in range(len(numbers)):
+        number = numbers[i]
+        complement = target_sum - number
+
+        if complement in memo: # hashmap has O(1) lookup
+            return (memo[complement], i)
+        
+        memo[number] = i
+
