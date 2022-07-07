066311e30511cca3065aa3beb7cb293f5cf9097e
Pim <pimvkeulen@gmail.com>
2022-07-07 19:09:27 +0200

    er: permutation

--- exhaustive recursion/permutation.py
+++ exhaustive recursion/permutation.py
@@ -1,15 +1,20 @@
-def create_combinations(items, k):
+def permutations(items, k=None):
+    if k is None:
+        k = len(items)
+
     if k == 0:
         return [[]]
+
 
     new_combinations = []
     for i in range(len(items)):
         item = items[i]
-        new_list = items[i+1:]
+        new_list = items[:i] + items[i+1:]
 
-        other_combinations = create_combinations(new_list, k-1)
+        other_combinations = permutations(new_list, k-1)
         for lst in other_combinations:
             lst.append(item)
             new_combinations.append(lst)
 
     return new_combinations
+
