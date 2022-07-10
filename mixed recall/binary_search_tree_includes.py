73e4ec2ac05fccdb415bb3fa35f56ca9c522113f
Pim <pimvkeulen@gmail.com>
2022-07-10 20:48:11 +0200

    mixed recall: bin search tree

---
+++ mixed recall/binary_search_tree_includes.py
@@ -0,0 +1,18 @@
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.left = None
+#         self.right = None
+
+
+def binary_search_tree_includes(root, target):
+    if root is None:
+        return False
+
+    if root.val == target:
+        return True
+
+    if root.val < target:
+        return binary_search_tree_includes(root.right, target)
+    if root.val > target:
+        return binary_search_tree_includes(root.left, target)
