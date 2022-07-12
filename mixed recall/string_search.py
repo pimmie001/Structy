f1e125a5ec1850dbd01348751d74947397b8dcee
Pim <pimvkeulen@gmail.com>
2022-07-12 22:14:51 +0200

    fix: string search

--- mixed recall/string_search.py
+++ mixed recall/string_search.py
@@ -1,8 +1,7 @@
 def string_search(grid, string):
-    memo = {}
     for y in range(len(grid)):
         for x in range(len(grid[0])):
-            if search(grid, string, y, x, memo=memo):
+            if search(grid, string, y, x):
                 return True
     return False
 
@@ -23,12 +22,16 @@ def search(grid, string, y, x, ind=0, memo={}):
         return memo[key]
 
 
+    char = grid[y][x]
+    grid[y][x] = '*'
+
     for move in [(0,1), (1,0), (0,-1), (-1,0)]:
         dx, dy = move
         if search(grid, string, y+dy, x+dx, ind+1, memo):
             memo[key] = True
             return True
-    
+
+    grid[y][x] = char
     memo[key] = False
     return False
 
